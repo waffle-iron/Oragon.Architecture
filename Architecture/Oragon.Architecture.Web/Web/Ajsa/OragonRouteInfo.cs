dd84872a75b30d308a56f43f1265a4492f34458d
luizcarlosfaria <luizcarlosfaria@e90c0fe1-8c64-4d05-b9cf-2d6f3e182000>
2013-09-10 07:57:20 +0000

    Acerto no OragonRouteInfo
    git-svn-id: https://subversion.assembla.com/svn/oragon.architecture/trunk@21 e90c0fe1-8c64-4d05-b9cf-2d6f3e182000

--- Architecture/Oragon.Architecture.Web/Web/Ajsa/OragonRouteInfo.cs
+++ Architecture/Oragon.Architecture.Web/Web/Ajsa/OragonRouteInfo.cs
@@ -9,58 +9,36 @@ namespace Oragon.Architecture.Web.Ajs
 	{
 		private System.Web.Mvc.Controller controller;
 
-		public OragonRouteInfo(System.Web.Mvc.Controller controller)
+		public OragonRouteInfo(System.Web.Mvc.Controller controller, string appBaseUrl = null)
 		{
 			this.controller = controller;
+
+			if(string.IsNullOrWhiteSpace(appBaseUrl))
+				this.AppBaseUrl = string.Format("{0}://{1}{2}", 
+					this.controller.Request.Url.Scheme, 
+					this.controller.Request.Url.Authority, 
+					this.controller.Url.Content("~/")
+				);
+			else
+				this.AppBaseUrl = appBaseUrl;
+
+			this.Area = (string)this.controller.RouteData.DataTokens["area"];
+			this.Controller = (string)this.controller.RouteData.Values["controller"];
+			this.Action = (string)this.controller.RouteData.Values["action"];
+			this.ID = (string)this.controller.RouteData.Values["id"];
+			this.RedirectTo = controller.HttpContext.Request.QueryString["ReturnUrl"];
 		}
 
-		public string RedirectTo
-		{
-			get 
-			{
-				string valueOfReturnUrl = controller.HttpContext.Request.QueryString["ReturnUrl"];
-				return valueOfReturnUrl;
-			}
-		}
+		public string RedirectTo { get; private set; }
 
-		public string AppBaseUrl
-		{
-			get
-			{
-				return   string.Format("{0}://{1}{2}", this.controller.Request.Url.Scheme, this.controller.Request.Url.Authority,  this.controller.Url.Content("~/"));
-			}
-		}
+		public string AppBaseUrl { get; private set; }
 
-		public string Area
-		{
-			get
-			{
-				return (string)this.controller.RouteData.DataTokens["area"];
-			}
-		}
+		public string Area { get; private set; }
 
-		public string Controller
-		{
-			get
-			{
-				return (string)this.controller.RouteData.Values["controller"];
-			}
-		}
+		public string Controller { get; private set; }
 
-		public string Action
-		{
-			get
-			{
-				return (string)this.controller.RouteData.Values["action"];
-			}
-		}
+		public string Action { get; private set; }
 
-		public string ID
-		{
-			get
-			{
-				return (string)this.controller.RouteData.Values["id"];
-			}
-		}
+		public string ID { get; private set; }
 	}
 }
