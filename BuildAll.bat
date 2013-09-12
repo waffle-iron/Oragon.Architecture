187a35cf21ce96c585a0364eda61de52ed37ace1
luizcarlosfaria <luizcarlosfaria@e90c0fe1-8c64-4d05-b9cf-2d6f3e182000>
2013-09-12 14:21:03 +0000

    Rebuild completo das soluções
    git-svn-id: https://subversion.assembla.com/svn/oragon.architecture/trunk@23 e90c0fe1-8c64-4d05-b9cf-2d6f3e182000

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,27 +1,14 @@
 echo 
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "Aaron\Oragon.Dynamics.sln"
-pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "EnterpriseLog\Oragon Log.sln"
-pause
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "Architecture\Oragon Architecture.sln"
 pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "EnterpriseLog\Oragon Log.sln"
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "EnterpriseLog\Oragon Architecture LogEngine.sln"
 pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "GeradorDeCodigo\Oragon.CodeGeneration.sln"
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "CodeGen\Oragon.CodeGen.sln"
 pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "JenkinsTasks\JenkinsTasks.sln"
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "JenkinsTasks\Oragon.JenkinsTasks.sln"
 pause
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "Replacer\Replacer.sln"
 pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "TalkToMe\TalkToMe.sln"
-pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "examples\Oragon.Sample01\Oragon.Sample01.sln"
-pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "examples\Oragon.Sample01\Oragon.Sample01.sln"
-pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "examples\Oragon.Exemplo02\Oragon.Exemplo02.sln"
-pause
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:rebuild "examples\Oragon.Exemplo03\Oragon.Exemplo03.sln"
-pause
+echo off
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Finalizando... !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
 pause
