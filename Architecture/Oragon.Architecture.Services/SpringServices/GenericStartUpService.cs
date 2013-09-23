f332c2b66873cdf2276e33b3192a08ef294e56a4
luizcarlosfaria <luizcarlosfaria@e90c0fe1-8c64-4d05-b9cf-2d6f3e182000>
2013-09-23 21:32:01 +0000

    Adição de novo comportamento no GenericStartUpService, para suportar múltiplas instâncias em, listas, encadeadas.
    git-svn-id: https://subversion.assembla.com/svn/oragon.architecture/trunk@25 e90c0fe1-8c64-4d05-b9cf-2d6f3e182000

--- Architecture/Oragon.Architecture.Services/SpringServices/GenericStartUpService.cs
+++ Architecture/Oragon.Architecture.Services/SpringServices/GenericStartUpService.cs
@@ -24,6 +24,12 @@ namespace Oragon.Architecture.Services.SpringServices
 		/// </summary>
 		public object HandlerObject { get; set; }
 
+
+		/// <summary>
+		/// Objeto responsável pelo startup EntryPoint
+		/// </summary>
+		public List<object> HandlerObjects { get; set; }
+
 		/// <summary>
 		/// Método (void) a ser chamado durante a inicialização
 		/// </summary>
@@ -40,7 +46,7 @@ namespace Oragon.Architecture.Services.SpringServices
 		public void Start()
 		{
 			if (string.IsNullOrWhiteSpace(this.StartMethodName) == false)
-				this.CallMethod(this.StartMethodName);
+				this.CallMethod(this.StartMethodName, Order.Desc);
 		}
 
 		/// <summary>
@@ -49,16 +55,37 @@ namespace Oragon.Architecture.Services.SpringServices
 		public void Stop()
 		{
 			if (string.IsNullOrWhiteSpace(this.StopMethodName) == false)
-				this.CallMethod(this.StopMethodName);
+				this.CallMethod(this.StopMethodName, Order.Desc);
 		}
 
 		/// <summary>
 		/// Executa a chamada ao método especificado
 		/// </summary>
 		/// <param name="methodName"></param>
-		private void CallMethod(string methodName)
+		private void CallMethod(string methodName, Order orderToExecute)
 		{
-			Spring.Expressions.ExpressionEvaluator.GetValue(this.HandlerObject, methodName + "()", null);
+			if (this.HandlerObjects == null)
+				this.HandlerObjects = new List<object>();
+
+			if (this.HandlerObject != null && this.HandlerObjects.Contains(this.HandlerObject) == false)
+				this.HandlerObjects.Insert(0, this.HandlerObject);
+
+			if (this.HandlerObjects.Count == 0)
+				throw new InvalidOperationException("Não há itens configurados para a execução. Use as propriedades HandlerObject ou HandlerObjects");
+
+			IEnumerable<object> listToExecute = orderToExecute == Order.Asc ? this.HandlerObjects : new List<object>(this.HandlerObjects).Reverse<object>();
+
+			foreach (var currentObject in listToExecute)
+			{
+				Spring.Expressions.ExpressionEvaluator.GetValue(currentObject, methodName + "()", null);
+			}
 		}
+
+		private enum Order
+		{
+			Asc,
+			Desc
+		}
+
 	}
 }
