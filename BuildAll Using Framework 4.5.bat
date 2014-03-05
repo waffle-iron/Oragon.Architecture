73826a8fc9aa999c2d592822d62172681ff1ef72
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2014-03-05 20:10:15 -0300

    Update da plataforma acertando problemas no build

--- BuildAll Using Framework 4.5.bat
+++ BuildAll Using Framework 4.5.bat
@@ -1,11 +1,11 @@
 echo 
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /t:rebuild "Architecture\Oragon Architecture.4.5.sln"
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /target:rebuild /property:Configuration=Release "Architecture\Oragon Architecture.4.5.sln"
 pause
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /t:rebuild "EnterpriseLog\Oragon Architecture LogEngine.4.5.sln"
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /target:rebuild /property:Configuration=Release "EnterpriseLog\Oragon Architecture LogEngine.4.5.sln"
 pause
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /t:rebuild "CodeGen\Oragon.CodeGen.4.5.sln"
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /target:rebuild /property:Configuration=Release "CodeGen\Oragon.CodeGen.4.5.sln"
 pause
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /t:rebuild "JenkinsTasks\Oragon.JenkinsTasks.4.5.sln"
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /target:rebuild /property:Configuration=Release "JenkinsTasks\Oragon.JenkinsTasks.4.5.sln"
 pause
 echo off
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Finalizando... !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
