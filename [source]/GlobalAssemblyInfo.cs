35ec24bb7762c2ebad01e0cb2d653177621c04dd
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2014-04-17 23:10:04 -0300

    Atualização do GitIgnore e adição do projeto base Oragon Architecture

---
+++ [source]/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Oragon.Architecture")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
