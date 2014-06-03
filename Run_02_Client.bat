135a2fec638d8e449be4ad7aae3bf77383aa87c8
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2014-06-03 09:23:00 -0300

    Atualização dos pacotes estáticos

--- Run_02_Client.bat
+++ Run_02_Client.bat
@@ -1,2 +1,6 @@
 cd "[source]\Oragon.Architecture.ApplicationHosting.HostProcess\bin\Debug\"
 Oragon.Architecture.ApplicationHosting.HostProcess.exe console -serviceconfigurationfile "..\..\..\..\Examples\ApplicationHostingNinjectExample\ApplicationHostingExample.xml"
+cd..
+cd..
+cd..
+cd..
