bfab15f773e04339e225e35102478e52aa027544
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2014-07-27 12:08:14 -0300

    Atualização no Build.ps1 e GlobalAssemblyInfo

--- Build.ps1
+++ Build.ps1
@@ -1,35 +1,14 @@
-﻿if(${env:ProgramFiles(x86)} -eq $null)
-{
-    $msBuildFile =  ${env:ProgramFiles} + "\MSBuild\12.0\Bin\MSBuild.exe"  
-}
-else
-{
-    $msBuildFile =  ${env:ProgramFiles(x86)} + "\MSBuild\12.0\Bin\MSBuild.exe"  
-}
+﻿$workingDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition;
+$sourceDirectory = [System.IO.Path]::Combine($workingDirectory, "[Source]")
+$buildDirectory = [System.IO.Path]::Combine($workingDirectory, "[Build]")
+$AssemblyInfoVersionManager = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.AssemblyInfoVersionManager.exe")
+$NugetTools = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.NugetTools.exe")
+$ResourcePack = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.ResourcePack.exe")
+$GlobalAssemblyInfo = [System.IO.Path]::Combine($sourceDirectory, "GlobalAssemblyInfo.cs")
+$NuGet = [System.IO.Path]::Combine($sourceDirectory, ".nuget", "NuGet.exe")
+$solutionFile = [System.IO.Path]::Combine($sourceDirectory, "OragonArchitecture.sln")
 
 
-$workingDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition;
-$NugetTools = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.NugetTools.exe");
-$solutionFile = [System.IO.Path]::Combine($workingDirectory, "[Source]", "OragonArchitecture.sln");
-$buildDirectory = [System.IO.Path]::Combine($workingDirectory, "[Build]");
-$designDirectory = [System.IO.Path]::Combine($workingDirectory, "[Design]");
-$oldDirectory = [System.IO.Path]::Combine($workingDirectory, "[OLD]");
-$referencesDirectory = [System.IO.Path]::Combine($workingDirectory, "[References]");
-$referencesDirectory = [System.IO.Path]::Combine($workingDirectory, "[References]");
-$sourceDirectory = [System.IO.Path]::Combine($workingDirectory, "[Source]");
-$nugetFile = [System.IO.Path]::Combine($workingDirectory, "[Source]", ".nuget", "NuGet.exe");
-$solutionFile = [System.IO.Path]::Combine($workingDirectory, "[Source]", "OragonArchitecture.sln");
-$nugetToolsFile =  [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.NugetTools.exe");
-$resourcePackFile = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.ResourcePack.exe");
-$AssemblyInfoVersionManagerFile = [System.IO.Path]::Combine($workingDirectory, "[Tools]", "BuildTools", "Oragon.BuildTools.AssemblyInfoVersionManager.exe");
-        
-
-
-function GetBuildFiles
-{
-    $returnValue = [System.IO.Directory]::GetFiles($buildDirectory, "*.nupkg")
-    return $returnValue
-}
 
 function myeval($command) 
 {
@@ -44,33 +23,39 @@ function myeval($command)
     }
 }
 
-
-##################################################################################################################################
-##################################################################################################################################
-##################################################################################################################################
-
-
-Write-Host "*************************************************************************"
-Write-Host "Removendo do build anterior"
-Write-Host "*************************************************************************"
-$oldBuildFiles = GetBuildFiles
-foreach($oldBuildFile in $oldBuildFiles)
+if(${env:ProgramFiles(x86)} -eq $null)
 {
-    [System.IO.File]::Delete($oldBuildFile)
+    $msBuildFile =  ${env:ProgramFiles} + "\MSBuild\12.0\Bin\MSBuild.exe"  
+}
+else
+{
+    $msBuildFile =  ${env:ProgramFiles(x86)} + "\MSBuild\12.0\Bin\MSBuild.exe"  
 }
 
+function GetNuSpecFiles
+{
+    $returnValue = [System.IO.Directory]::GetFiles($sourceDirectory, "*.nuspec", [System.IO.SearchOption]::AllDirectories)
+    return $returnValue
+}
 
+function FormatNuGetBuildCommand
+{
+    param ([string]$nuSpecFile)
+    $command = """$NuGet"" pack ""$nuSpecFile"" -Build -Prop Configuration=Release -IncludeReferencedProjects -NoPackageAnalysis -Version 7.0.0-GA -OutputDirectory ""$buildDirectory"""
+    return $command
+}
 
-Write-Host "*************************************************************************"
-Write-Host "Gerando Resources"
-Write-Host "*************************************************************************"
-myeval("""$resourcePackFile"" --pack --ProjectGuid=30E4014D-8E36-4E02-AE1A-CD4EB1718683 --RootNamespace=Oragon.Resources.Bootstrap --AssemblyName=Oragon.Resources.Bootstrap --Path=$sourceDirectory\Oragon.Resources.Bootstrap --TargetFrameworkVersion=v4.5.1 --Version=$version --VersionTag=$versionTag")
+myeval("""$ResourcePack"" --pack --ProjectGuid=30E4014D-8E36-4E02-AE1A-CD4EB1718683 --RootNamespace=Oragon.Resources.Bootstrap --AssemblyName=Oragon.Resources.Bootstrap --Path=$sourceDirectory\Oragon.Resources.Bootstrap --TargetFrameworkVersion=v4.5.1 --Version=$env:GlobalAssemblyVersion --VersionTag=GA")
 
-Write-Host "Realizando Build da Solução"
+myeval("""$AssemblyInfoVersionManager"" --File=""$GlobalAssemblyInfo"" --Version=7.0.0 --VersionTag=GA")
+
+myeval("""$NugetTools"" --mergenuspec --solution=""$solutionFile"" --createNuspecIfNeed=""$NuGet"" --convertSolutionProjectsInNugetReferences  ")
+
 myeval("""$msBuildFile"" /target:rebuild /property:Configuration=Release ""$solutionFile""")
 
-Write-Host "*************************************************************************"
-Write-Host "Atualizando NuSpecs da solução"
-Write-Host "*************************************************************************"
-myeval("""$nugetToolsFile"" --mergenuspec --solution=""$solutionFile"" --createNuspecIfNeed=""$nugetFile"" --convertSolutionProjectsInNugetReferences  ")
-
+$nuSpecFiles = GetNuSpecFiles
+foreach($nuSpecFile in $nuSpecFiles)
+{
+    $command = FormatNuGetBuildCommand($nuSpecFile)
+    myeval($command)
+}
