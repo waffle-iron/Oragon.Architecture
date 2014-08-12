b9a8dd646f7a9a8f49411b5a1fb8456cf7edf19a
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2014-08-12 04:58:28 -0300

    Merge dos Projetos Serialization, Networking com o Oragon.Architecture.

--- [source]/Oragon.Architecture.Networking/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Oragon.Architecture.Networking")]
-[assembly: AssemblyDescription("Oragon Architecture Application Framework Networking Helpers")]
-[assembly: ComVisible(false)]
-[assembly: Guid("d5c79994-2ed8-4f17-a8e5-aeff29682671")]
